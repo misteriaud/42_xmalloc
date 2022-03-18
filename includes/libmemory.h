84424b1fc5aa3007ba9ddfbc591065624c2359db
Maxime Riaud (Home) <mriaud@student.42.fr>
2022-03-18 13:09:52 +0100

    fix erreur header

--- includes/libmemory.h
+++ includes/libmemory.h
@@ -6,7 +6,7 @@
 /*   By: mriaud <mriaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 20:35:25 by mriaud            #+#    #+#             */
-/*   Updated: 2022/03/18 11:47:25 by mriaud           ###   ########.fr       */
+/*   Updated: 2022/03/18 13:09:33 by mriaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,18 @@ typedef struct s_node {
 }				t_node;
 
 // ft_bzero.c && ft_memcpy.c
-static void	ft_bzero(void *s, size_t n);
-static void	ft_memcpy(void *dest, const void *src, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	ft_memcpy(void *dest, const void *src, size_t n);
 
 // t_node.c
-static t_node	**get_first_node(void);
-static t_node	*new_node(t_node **first, int group);
-static t_node	*get_node(int group);
+t_node	**get_first_node(void);
+t_node	*new_node(t_node **first, int group);
+t_node	*get_node(int group);
 
 // t_alloc.c
-static t_alloc	**get_first_alloc(int group);
-static t_alloc	*new_alloc(t_alloc **first, size_t size);
-static void	xfree_allocs(t_alloc *first);
+t_alloc	**get_first_alloc(int group);
+t_alloc	*new_alloc(t_alloc **first, size_t size);
+void	xfree_allocs(t_alloc *first);
 
 // libmemory.c
 int		xmalloc(void *ptr, size_t size, int group);
